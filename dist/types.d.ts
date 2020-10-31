c172b58d611e6d84628d57d64db3aec6df1200c6
Matthew Clive <arcticlight@arcticlight.me>
2020-10-30 22:54:47 -0400

    Purge typescript

--- dist/types.d.ts
+++
@@ -1,7 +0,0 @@
-export declare type InterpolationFunction<T> = (from: T, to: T, much: number) => T;
-export declare type EasingFunction = (x: number) => number;
-export declare type Newable = new (...x: any[]) => any;
-export interface TweenTypeRegistration<T> {
-    type: any;
-    interpFun: InterpolationFunction<T>;
-}
